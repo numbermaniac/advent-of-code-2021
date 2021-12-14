1f54f0a7ec79e2373e0e040e1e2c3b1664b4ebcc
lleytonj5 <5206120+numbermaniac@users.noreply.github.com>
2021-12-14 22:53:20 +1100

    day 7 cleanup & alternative solution

--- solutions/Day07.jl
+++ solutions/Day07.jl
@@ -1,7 +1,7 @@
 function part1()
 	crabs = [parse(Int, i) for i in split(readline("inputD7.txt"), ",")]
 	highest = maximum(crabs)
-	minfuel = minimum((n -> sum(abs(n - c) for c in crabs)), 1:highest)
+	minfuel = minimum(n -> sum(abs(n - c) for c in crabs), 1:highest)
 
 	return minfuel
 end
@@ -10,6 +10,15 @@ function part2()
 	crabs = [parse(Int, i) for i in split(readline("inputD7.txt"), ",")]
 	highest = maximum(crabs)
 	minfuel = minimum((n -> sum(sum(1:abs(n - c)) for c in crabs)), 1:highest)
+
+	return minfuel
+end
+
+function part1_broadcasted()
+	# An alternative solution to part1 that uses broadcasting instead of for-loops
+	crabs = parse.(Int, split(readline("inputD7.txt"), ","))
+	highest = maximum(crabs)
+	minfuel = minimum(n -> sum(abs.(n .- crabs)), 1:highest)
 
 	return minfuel
 end
